ea78748ef60dcc73a4ca8899a31502149e9d5e5a
67688FEA <359952820@qq.com>
2018-07-19 17:27:13 +0800

    update the ui

---
+++ Assets/Scripts/UI/BackgroundController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+
+    public bool change = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void LateUpdate()
+    {
+        if (change)
+        {
+
+        }
+    }
+
+}
--- Assets/Scripts/UI/HideSeekMainMenu.cs
+++ Assets/Scripts/UI/HideSeekMainMenu.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class HideSeekMainMenu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject title;
+    public GameObject Main;
+
+    public GameObject charactorChoose;
+    public GameObject charactorDescription;
+    public GameObject charactorChooseButton;
+
+    public Transform defaultPoint;
+    public Transform charactorPoint;
+    public GameObject background;
+
+    public void OnCharactorChooseClick()
+    {
+        title.SetActive(false);
+        Main.SetActive(false);
+        charactorChoose.SetActive(true);
+        charactorDescription.SetActive(true);
+        charactorChooseButton.SetActive(true);
+        background.transform.position = Vector3.Lerp(background.transform.position, charactorPoint.position, 2f);
+        background.transform.rotation = charactorPoint.rotation;
+
+    }
+    public void OnBackClick()
+    {
+        title.SetActive(true);
+        Main.SetActive(true);
+        charactorChoose.SetActive(false);
+        charactorDescription.SetActive(false);
+        charactorChooseButton.SetActive(false);
+        background.transform.position = Vector3.Lerp(background.transform.position, defaultPoint.position, 2f);
+        background.transform.rotation = defaultPoint.rotation;
+    }
+
 }
